5a310383069354be78077f0e5917c97c15ec06c2
buivantho <buivantho2601@gmail.com>
2021-01-15 12:48:16 +0700

    push

--- src/Components/Header/Content/Content.js
+++ src/Components/Header/Content/Content.js
@@ -14,42 +14,50 @@ const firebaseConfig = {
 const database = firebase.firestore();
 const dataProducts = [];
 class Content extends Component {
- 
+  constructor(props) {
+    super(props);
+    
+    this.state = { isLoading: true, obj :[] ,value: 0 }
+
+  }
   getData = () => {
+    const { obj } = this.state
     database.collection('products').doc('327966332').collection('datas').get()
   .then(response => {
     
     console.log(response)
     // console.log(response.)
+  
     response.forEach(document => {
       dataProducts.push(document.data())
-      // test.push(document.data())
       console.log(dataProducts)
     })
-    
-  })
+    this.setState({obj: dataProducts})
+  }
+  
+  )
 
 
   }
-  
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    console.log(this.state.value)
+  }
+
+    
     handleClick = (index) => {
       
-      var x = document.getElementById("value").value;
+      // var x = document.getElementById("value").value;
 // var tes = document.getElementsByClassName
 // document.getElementById("inlineFormCustomSelect").value = 1
-        console.log(x);
-        console.log(index);
-database.collection('products').doc('327966332').collection('datas').doc('uZbt4H8mamYV4CC37EVE').update({
-    status : x*1
-})
+        // console.log(x);
+        // console.log(index);
+// database.collection('products').doc('327966332').collection('datas').doc('uZbt4H8mamYV4CC37EVE').update({
+//     status : x*1
+// })
 
       }
-    constructor(props) {
-        super(props);
-        
-        this.state = { isLoading: true }
-
-      }
+   
       componentDidMount() {
        
         this.getData();
@@ -67,7 +75,7 @@ database.collection('products').doc('327966332').collection('datas').doc('uZbt4H
 
  
     render() {
-    
+    console.log(this.state.obj)
         return (
           this.state.isLoading  ? <div><h1>loading ....</h1></div> :  <div>
               
@@ -105,18 +113,14 @@ database.collection('products').doc('327966332').collection('datas').doc('uZbt4H
           <div id="dataTable_wrapper" className="dataTables_wrapper dt-bootstrap4"><div className="row"><div className="col-sm-12 col-md-6"><div className="dataTables_length" id="dataTable_length"><label>Show 
             <select name="dataTable_length" aria-controls="dataTable" className="custom-select custom-select-sm form-control form-control-sm"><option value={10}>10</option><option value={25}>25</option><option value={50}>50</option><option value={100}>100</option></select> entries</label></div></div><div className="col-sm-12 col-md-6"><div id="dataTable_filter" className="dataTables_filter"><label>Search:<input type="search" className="form-control form-control-sm" placeholder aria-controls="dataTable" /></label></div></div></div><div className="row"><div className="col-sm-12"><table className="table table-bordered table-hover dataTable" id="dataTable" width="100%" cellSpacing={0} role="grid" aria-describedby="dataTable_info" style={{width: '100%'}}>
                   <thead>
-                  <tr><th rowSpan={1} colSpan={1}>Người đặt</th><th rowSpan={1} colSpan={1}>Địa chỉ</th><th rowSpan={1} colSpan={1}>Số điện thoại</th><th rowSpan={1} colSpan={1}>Giá đơn hàng</th><th rowSpan={1} colSpan={1}>Thời gian giao</th><th rowSpan={1} colSpan={1}>Thang toán</th><th rowSpan={1} colSpan={1}>Xác nhận đơn <select className="custom-select mr-sm-2" id="value" class="testss">
-                    <option selected value={0}>Chưa xác Nhận </option>
-                    <option value={1}>Đã thu tiền </option>
-                    <option value={2}>Đang giao</option>
-                    <option value={3}>Đã giao</option>
-                    </select></th><th rowSpan={1} colSpan={1}>Status</th><th rowSpan={1} colSpan={1}> <button onClick={this.handleClick} className="btn btn-datatable btn-icon btn-transparent-dark"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2"><polyline points="3 6 5 6 21 6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><line x1={10} y1={11} x2={10} y2={17} /><line x1={14} y1={11} x2={14} y2={17} /></svg></button></th></tr>
+                  <tr><th rowSpan={1} colSpan={1}>Người đặt</th><th rowSpan={1} colSpan={1}>Địa chỉ</th><th rowSpan={1} colSpan={1}>Số điện thoại</th><th rowSpan={1} colSpan={1}>Giá đơn hàng</th><th rowSpan={1} colSpan={1}>Thời gian giao</th><th rowSpan={1} colSpan={1}>Thang toán</th><th rowSpan={1} colSpan={1}>Xác nhận đơn 
+                  </th><th rowSpan={1} colSpan={1}>Status</th><th rowSpan={1} colSpan={1}> <button onClick={this.handleClick} className="btn btn-datatable btn-icon btn-transparent-dark"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2"><polyline points="3 6 5 6 21 6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><line x1={10} y1={11} x2={10} y2={17} /><line x1={14} y1={11} x2={14} y2={17} /></svg></button></th></tr>
                   </thead>
                   {/* <tfoot>
                     <tr><th rowSpan={1} colSpan={1}>Name</th><th rowSpan={1} colSpan={1}>Position</th><th rowSpan={1} colSpan={1}>Office</th><th rowSpan={1} colSpan={1}>Age</th><th rowSpan={1} colSpan={1}>Start date</th><th rowSpan={1} colSpan={1}>Salary</th><th rowSpan={1} colSpan={1}>Status</th><th rowSpan={1} colSpan={1}>Actions</th></tr>
                   </tfoot> */}
                   <tbody>
-                  {dataProducts.map((data,i) =>(
+                  {this.state.obj.map((data,i) =>(
                     <tr role="row" className="odd">
                       <td className="sorting_1">{data.full_name}</td>
                   <td>{data.address}</td>
@@ -124,7 +128,7 @@ database.collection('products').doc('327966332').collection('datas').doc('uZbt4H
                   <td>{data.price}</td>
                   <td>{data.time_oder}</td>
                   <td>{data.paymentType}</td>
-                  <td><select className="custom-select mr-sm-2" id="value" class="testss">
+                  <td><select className="custom-select mr-sm-2"  value={this.state.value} onChange={this.handleChange} >
                     <option selected value={0}>Chưa xác Nhận </option>
                     <option value={1}>Đã thu tiền </option>
                     <option value={2}>Đang giao</option>
