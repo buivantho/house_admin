82add6e4ce7580e082ee7ea8172ea0d22081b71b
MAC <mac@MacBook-Pro-cua-MAC.local>
2021-01-16 23:26:22 +0700

    list

--- src/Components/Header/Content/Content.js
+++ src/Components/Header/Content/Content.js
@@ -13,53 +13,98 @@ const firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
 const database = firebase.firestore();
 const dataProducts = [];
+const dataId = [];
+const dataUserPhone =[];
 class Content extends Component {
   constructor(props) {
     super(props);
     
-    this.state = { isLoading: true, obj :[] ,value: 0 }
+    this.state = { 
+      isLoading: true, 
+      obj :[] ,
+      objId :[] ,
+      objUserPhone:[],
+      value: 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
 
+  }
+  getdatauser = () =>{
+    database.collection('users').get()
+    .then(response => {
+    
+        console.log(response)
+        console.log("response")
+        // // console.log(response.)
+      
+        response.forEach(document => {
+          // const dataUserPhone = [];
+          console.log(document.id)
+          // dataUserPhone.push()
+          database.collection('products').doc(document.id).collection('datas').get()
+          .then(response => {
+            
+            console.log(response)
+            // console.log(response.)
+          
+            response.forEach(document => {
+              
+              dataProducts.push(document.data())
+              dataId.push(document.id)
+              console.log(document.id)
+            })
+            this.setState({obj: dataProducts})
+            this.setState({objId: dataId})
+          }
+          
+          )
+        //   dataId.push(document.id)
+        //   console.log(document.id)
+        })
+        // this.setState({objUserPhone: dataUserPhone})
+        // this.setState({objId: dataId})
+      })
   }
   getData = () => {
+   for(var i=0;i<this.state.objUserPhone.length;i++){
+     console.log(this.state.objUserPhone)
+   }
     const { obj } = this.state
-    database.collection('products').doc('327966332').collection('datas').get()
-  .then(response => {
     
-    console.log(response)
-    // console.log(response.)
   
-    response.forEach(document => {
-      dataProducts.push(document.data())
-      console.log(dataProducts)
+
+
+  }
+  // handleChange(event) {
+  //   console.log(event)
+
+  // }
+  handleChange(event) {    this.setState({value: event.target.value});}
+  handleClick(key,data){
+    console.log(data.phoneNumber)
+    database.collection('products').doc(data.phoneNumber).collection('datas').doc(this.state.objId[key]).update({
+      status : this.state.value*1
     })
-    this.setState({obj: dataProducts})
-  }
-  
-  )
-
-
-  }
-  handleChange(event) {
-    this.setState({value: event.target.value});
+    console.log(key)
     console.log(this.state.value)
-  }
 
-    
-    handleClick = (index) => {
-      
-      // var x = document.getElementById("value").value;
-// var tes = document.getElementsByClassName
-// document.getElementById("inlineFormCustomSelect").value = 1
-        // console.log(x);
-        // console.log(index);
-// database.collection('products').doc('327966332').collection('datas').doc('uZbt4H8mamYV4CC37EVE').update({
-//     status : x*1
-// })
+}
+//     handleClick = (index) => {
+//       console.log(index)
+//       console.log("index")
+//       // var x = document.getElementById("value").value;
+// // var tes = document.getElementsByClassName
+// // document.getElementById("inlineFormCustomSelect").value = 1
+//         // console.log(x);
+//         // console.log(index);
+// // database.collection('products').doc('327966332').collection('datas').doc('uZbt4H8mamYV4CC37EVE').update({
+// //     status : x*1
+// // })
 
-      }
+//       }
    
       componentDidMount() {
-       
+       this.getdatauser();
         this.getData();
         console.log(dataProducts)
         // setInterval(() => {
@@ -76,6 +121,8 @@ class Content extends Component {
  
     render() {
     console.log(this.state.obj)
+    console.log(this.state.objId[0])
+    console.log(this.state.objUserPhone)
         return (
           this.state.isLoading  ? <div><h1>loading ....</h1></div> :  <div>
               
@@ -111,24 +158,26 @@ class Content extends Component {
           {dataProducts == [] ? <h1>Loading</h1> : 
           <div className="datatable">
           <div id="dataTable_wrapper" className="dataTables_wrapper dt-bootstrap4"><div className="row"><div className="col-sm-12 col-md-6"><div className="dataTables_length" id="dataTable_length"><label>Show 
-            <select name="dataTable_length" aria-controls="dataTable" className="custom-select custom-select-sm form-control form-control-sm"><option value={10}>10</option><option value={25}>25</option><option value={50}>50</option><option value={100}>100</option></select> entries</label></div></div><div className="col-sm-12 col-md-6"><div id="dataTable_filter" className="dataTables_filter"><label>Search:<input type="search" className="form-control form-control-sm" placeholder aria-controls="dataTable" /></label></div></div></div><div className="row"><div className="col-sm-12"><table className="table table-bordered table-hover dataTable" id="dataTable" width="100%" cellSpacing={0} role="grid" aria-describedby="dataTable_info" style={{width: '100%'}}>
+            <select name="dataTable_length" aria-controls="dataTable" className="custom-select custom-select-sm form-control form-control-sm"><option value={10}>10</option><option value={25}>25</option><option value={50}>50</option><option value={100}>100</option></select> entries</label></div></div><div className="col-sm-12 col-md-6"><div id="dataTable_filter" className="dataTables_filter">
+              <label>Search:<input type="search"  value={this.state.query}
+            onChange={this.handleInputChange} className="form-control form-control-sm" placeholder aria-controls="dataTable" /></label></div></div></div><div className="row"><div className="col-sm-12"><table className="table table-bordered table-hover dataTable" id="dataTable" width="100%" cellSpacing={0} role="grid" aria-describedby="dataTable_info" style={{width: '100%'}}>
                   <thead>
                   <tr><th rowSpan={1} colSpan={1}>Người đặt</th><th rowSpan={1} colSpan={1}>Địa chỉ</th><th rowSpan={1} colSpan={1}>Số điện thoại</th><th rowSpan={1} colSpan={1}>Giá đơn hàng</th><th rowSpan={1} colSpan={1}>Thời gian giao</th><th rowSpan={1} colSpan={1}>Thang toán</th><th rowSpan={1} colSpan={1}>Xác nhận đơn 
-                  </th><th rowSpan={1} colSpan={1}>Status</th><th rowSpan={1} colSpan={1}> <button onClick={this.handleClick} className="btn btn-datatable btn-icon btn-transparent-dark"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2"><polyline points="3 6 5 6 21 6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><line x1={10} y1={11} x2={10} y2={17} /><line x1={14} y1={11} x2={14} y2={17} /></svg></button></th></tr>
+                  </th><th rowSpan={1} colSpan={1}>Status</th><th rowSpan={1} colSpan={1}> ss</th></tr>
                   </thead>
                   {/* <tfoot>
                     <tr><th rowSpan={1} colSpan={1}>Name</th><th rowSpan={1} colSpan={1}>Position</th><th rowSpan={1} colSpan={1}>Office</th><th rowSpan={1} colSpan={1}>Age</th><th rowSpan={1} colSpan={1}>Start date</th><th rowSpan={1} colSpan={1}>Salary</th><th rowSpan={1} colSpan={1}>Status</th><th rowSpan={1} colSpan={1}>Actions</th></tr>
                   </tfoot> */}
                   <tbody>
                   {this.state.obj.map((data,i) =>(
-                    <tr role="row" className="odd">
+                    <tr role="row" className="odd" key={i}> 
                       <td className="sorting_1">{data.full_name}</td>
                   <td>{data.address}</td>
                   <td>{data.phoneNumber}</td>
                   <td>{data.price}</td>
                   <td>{data.time_oder}</td>
                   <td>{data.paymentType}</td>
-                  <td><select className="custom-select mr-sm-2"  value={this.state.value} onChange={this.handleChange} >
+                  <td><select className="custom-select mr-sm-2" value={this.state.value.id} onChange={this.handleChange}  >
                     <option selected value={0}>Chưa xác Nhận </option>
                     <option value={1}>Đã thu tiền </option>
                     <option value={2}>Đang giao</option>
@@ -137,7 +186,7 @@ class Content extends Component {
                   </td>
                   <td>{data.status == 0 ? "Chưa xác nhận" : data.status == 1 ? "Đã thu tiền" : data.status == 2 ? "Đang giao": data.status == 3 ? "Hoàn thành": ""}</td>
                       <td>
-                        <button onClick={this.handleClick(i)} className="btn btn-datatable btn-icon btn-transparent-dark"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2"><polyline points="3 6 5 6 21 6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><line x1={10} y1={11} x2={10} y2={17} /><line x1={14} y1={11} x2={14} y2={17} /></svg></button>
+                        <button onClick={()=>this.handleClick(i,data)} className="btn btn-datatable btn-icon btn-transparent-dark"><svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="feather feather-trash-2"><polyline points="3 6 5 6 21 6" /><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" /><line x1={10} y1={11} x2={10} y2={17} /><line x1={14} y1={11} x2={14} y2={17} /></svg></button>
                       </td>
                     </tr>
                      ))}
